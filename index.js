c8baeea5bcba6c0809d492f616d99c409542bf07
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-07-09 16:27:03 +0530

    Improved Speed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var preloader=document.getElementById("loading");
 var mode="light";
 function myfunction(){
+    var x=document.getElementsByClassName('mode');
+    var elem=x[0]
+    elem.innerHTML=`<link rel="stylesheet" href="styles2.css">`;
+    elem.innerHTML=`<link rel="stylesheet" href="styles.css">`;
     preloader.style.display="none";
 }
 
