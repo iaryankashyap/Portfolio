e8af0eac8c4df782aa279b9b83ba831b80c39d3d
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-07-09 16:23:46 +0530

    Added Dark Theme

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var preloader=document.getElementById("loading")
-
+var preloader=document.getElementById("loading");
+var mode="light";
 function myfunction(){
     preloader.style.display="none";
+}
+
+function theme(){
+    var x=document.getElementsByClassName('mode');
+    var elem=x[0]
+    if(mode=="light"){
+        elem.innerHTML=`<link rel="stylesheet" href="styles2.css">`;
+        mode="dark";
+    }
+    else{
+        elem.innerHTML=`<link rel="stylesheet" href="styles.css">`;
+        mode="light";
+    
+    }
 }
